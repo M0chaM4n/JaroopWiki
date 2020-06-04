9bb37e5226aa899ca77ea52b1ca6b27e56f9732f
MochaMan <amathews707@gmail.com>
2020-06-04 01:00:20 -0400

    Functionally completed, just wanna clean it up a bit.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,33 @@
+import java.io.IOException
+
 import scala.io.StdIn._
 import org.jsoup._
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
 object Main extends App
 {
   var query = new String
-  while (query == null)
-  {
-    println("What would you like to read about? Or type 'exit' to conclude.\n")
+
+    print("What would you like to read about?\n")
     query = scala.io.StdIn.readLine()
-    if (query == null)
+
+  if (query == null)
       println("No input given")
     else
     {
-
+      query.replace(" ","_")
+      val fullQuery =  "http://en.wikipedia.org/wiki/" + query
+      try
+        {
+          val doc = Jsoup.connect(fullQuery).get()
+          val body = doc.select("#mw-content-text")
+          val text  = body.select("p").not(".mw-empty-elt")
+          print(text.get(0).text())
+        }
+      catch { case _: Throwable => println("Could not find article.Terminating")}
+      System.exit(1)
     }
-  }
+
 }
+
